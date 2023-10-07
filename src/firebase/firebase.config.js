11fff9bea3275a761721e1d3cf4b7abe9492bbf2
Rahamat725 <skrahamatullah500@gmail.com>
2023-10-07 18:47:42 +0530

    added packedges which are needed for the project

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjPiK4-me6F5iEocCXTUJoovsCfrEufkQ",
+  authDomain: "corporatejunction-a6c73.firebaseapp.com",
+  projectId: "corporatejunction-a6c73",
+  storageBucket: "corporatejunction-a6c73.appspot.com",
+  messagingSenderId: "713082453839",
+  appId: "1:713082453839:web:5363db54160307cc8a0f3a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
